8d3fcc7a03a201c48b86f685b1fb590bda4e4089
Ebikeme Ese <ebikemeese257@gmail.com>
2026-03-12 14:34:15 +0100

    Refactor Loader component to use forwardRef

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,33 +1,9 @@
-// src/components/Loader.tsx
-import { useEffect, useRef } from "react";
-import gsap from "gsap";
+import { forwardRef } from "react";
 import "../assets/css/loader.css";
 
-const Loader = () => {
-  const loaderRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleLoad = () => {
-      // Animate loader sliding up
-      gsap.to(loaderRef.current, {
-        y: "-100%",       // slide up off screen
-        opacity: 0,       // fade out
-        duration: 1,      // 1 second animation
-        ease: "power3.inOut",
-        onComplete: () => {
-          if (loaderRef.current) {
-            loaderRef.current.style.display = "none"; // remove from flow
-          }
-        }
-      });
-    };
-
-    window.addEventListener("load", handleLoad);
-    return () => window.removeEventListener("load", handleLoad);
-  }, []);
-
+const Loader = forwardRef<HTMLDivElement>((props, ref) => {
   return (
-    <div ref={loaderRef} className="loader-container">
+    <div ref={ref} className="loader-container">
       <svg viewBox="0 0 400 160">
         <text
           x="50%"
@@ -38,6 +14,7 @@ const Loader = () => {
         >
           Welcome
         </text>
+
         <text
           x="50%"
           y="50%"
@@ -45,12 +22,10 @@ const Loader = () => {
           dx="1.7em"
           textAnchor="middle"
           className="dot"
-        >
-          
-        </text>
+        ></text>
       </svg>
     </div>
   );
-};
+});
 
 export default Loader;
