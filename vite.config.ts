b257e8cee2bcd182129e3aea00d8ef938ad6d3e9
Ebikemeese <ebikemeese257@gmail.com>
2026-08-07 05:44:17 +0100

    remove commented code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,29 +12,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
-
-
-// import path from "path"
-// import tailwindcss from "@tailwindcss/vite"
-// import react from "@vitejs/plugin-react"
-// import { defineConfig } from "vite"
-
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   server: {
-//     host: "0.0.0.0", // or a specific hostname
-//     allowedHosts: [
-//       "localhost",
-//       "shady-dallas-nonepochal.ngrok-free.dev",
-//       "mydomain.com",
-//       "sub.mydomain.com"
-//     ],
-//     port: 5173,
-//   },
-// })
+});
